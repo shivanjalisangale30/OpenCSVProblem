b5351982baf100de68bd88715f3f6237f7c03d06
shivanjalisangale30 <shivanjalisangale5456@gmail.com>
2019-11-29 18:11:01 +0530

    ADD : clear

--- src/main/java/com/opencsv/StateCensusAnalyser.java
+++ src/main/java/com/opencsv/StateCensusAnalyser.java
@@ -4,19 +4,21 @@ import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.Reader;
+import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
 
 public class StateCensusAnalyser {
     private static final String INDIAN_STATES_INFORMATION_FILE = "/home/admin1/Desktop/OpenCSVProblem/StateCode.csv";
+    private static final String INDIAN_STATES_INFORMATION_FILE1 = "/home/admin1/Desktop/OpenCSVProblem/StateCode1.csv";
 
-    public int findNumberOfRecord() throws IOException {
+    public int findNumberOfRecord() throws CsvStateException {
         int counter = 0;
         try {
-            BufferedReader reader = Files.newBufferedReader(Paths.get(INDIAN_STATES_INFORMATION_FILE));
+            BufferedReader reader = Files.newBufferedReader(Paths.get(INDIAN_STATES_INFORMATION_FILE1));
             CsvToBean<CSVStates> csvToBeanBuilder = new CsvToBeanBuilder(reader)
                     .withType(CSVStates.class)
                     .withIgnoreLeadingWhiteSpace(true)
@@ -26,9 +28,9 @@ public class StateCensusAnalyser {
                 CSVStates nextState = iterator.next();
                 counter++;
             }
+        } catch (FileNotFoundException e){
+            throw new CsvStateException(CsvStateException.ExceptionType.NO_SUCH_FILE , "No such file exist");
         } catch (IOException e) {
-            e.printStackTrace();
-        } catch (IllegalStateException e) {
             e.printStackTrace();
         }
         return counter;
--- src/test/java/com/opencsv/StateCensusAnalyserTest.java
+++ src/test/java/com/opencsv/StateCensusAnalyserTest.java
@@ -9,10 +9,19 @@ import static org.junit.Assert.assertEquals;
 
 public class StateCensusAnalyserTest {
     @Test
-    public void givenStateInformation_whenProperRead_matchRecords() throws IOException {
+    public void givenStateInformation_whenProperRead_matchRecords() throws IOException, CsvStateException {
         StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
         int result = stateCensusAnalyser.findNumberOfRecord();
-        assertEquals(37,result);
+        Assert.assertEquals(37, result);
+    }
 
+    @Test
+    public void givenStateInformation_whenImproperRead_shouldThrowException() {
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+        try {
+            stateCensusAnalyser.findNumberOfRecord();
+        } catch (CsvStateException e) {
+            Assert.assertEquals(CsvStateException.ExceptionType.NO_SUCH_FILE, e.type);
+        }
     }
 }
